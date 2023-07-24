4fee923012da3861e3535554c9b549ebfed53a40
“andrewdoak” <thedoak@gmail.com>
2023-07-24 15:32:41 -0500

    Modified global user name and email in git config

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // Check that script is connected
 // console.log('App is connected');
 
+// git config --global user.name “andrewdoak”
+// git config --global user.email "thedoak@gmail.com"
+
 ////////////////////////
 // p5.js TYPING GAME
 ////////////////////////
