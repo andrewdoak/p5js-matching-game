b443451a74c4f93ca85f4f573a4e332363ec2b5e
“andrewdoak” <thedoak@gmail.com>
2023-07-25 13:26:36 -0500

    Wireframe link, p5 Tutorial progress, notes, sketches (links) and PM roadmap

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
 // Check that script is connected
 // console.log('App is connected');
 
-// git config --global user.name “andrewdoak”
-// git config --global user.email "thedoak@gmail.com"
+
 
 ////////////////////////
 // p5.js TYPING GAME
 ////////////////////////
+
+
+// 7/25 PM
+// FIGMA WIREFRAME // 
+// https://www.figma.com/file/SrXO2VK3EPpqCB1WxtARhf/p5.js-Game?type=whiteboard&node-id=129-420&t=mSykA24IbvEJqwEC-0
+
+
+
+// 7/25 AM to lunch
+// TUTORIAL PROGRESS, NOTES, & SKETCHES // 
+// FROM: 
+// https://www.linkedin.com/learning/coding-for-visual-learners-learning-javascript-from-scratch-16124466?u=116852650
+
+// CHAPTERRS 1-2 - Setting up a sketch, color
+// https://editor.p5js.org/andrewdoak/sketches/CF2TUko9u
+
+// CHAPTER 3 - Operators and variables
+// Created shapes and animated them using variables, REFACTORED code with variables
+// https://editor.p5js.org/andrewdoak/sketches/T0-KJyPTa
+
+// CHAPTER 4 - Conditional Statements and Comparison Operators
+// Learned how to use conditionals and nested conditionals to limit and manipulate animation
+// https://editor.p5js.org/andrewdoak/sketches/Cgd11uJpX
+
+
+// 7/25 PM after lunch - ROADMAP
+// Finish Tutorial "Class"
+// Work on creating a basic Tic Tac Toe in p5 (my Backup)
+// STRETCH: get some boilerplate HTML/CSS and 'publish' either Tic Tac Toe, or a sketch to GitHub pages (I have learned how to do this, as well as how to publish the sketch on a site)
+
