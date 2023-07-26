b75233faef419be11a4d6e289d5046a9f93759b5
“andrewdoak” <thedoak@gmail.com>
2023-07-26 08:55:13 -0500

    Created sketches working with loops, random() and noise() functions in p5.js. Links in .js file comments.

--- script.js
+++ script.js
@@ -36,3 +36,24 @@
 // Work on creating a basic Tic Tac Toe in p5 (my Backup)
 // STRETCH: get some boilerplate HTML/CSS and 'publish' either Tic Tac Toe, or a sketch to GitHub pages (I have learned how to do this, as well as how to publish the sketch on a site)
 
+// CHAPTER 5 - Mouse events
+// Change background color when mouse is pressed
+// Create a 'Drawing" program that follow the mouse x and y position and responds visually to speed change
+// https://editor.p5js.org/andrewdoak/sketches/hefGm-1G9
+
+// CHAPTER 6 - Loops
+// Create a variable that controls circle diameter. Create a for loop that iterates over the horizontal axis with circles, then the vertical axis to result in the whole screen being filled with circles (spaced and distributed evenly across the canvas)
+// https://editor.p5js.org/andrewdoak/sketches/wPNx5QPV4
+
+// CHAPTER 6, SECTION C - Loops (random() function)
+// Use the random() p5.js function to create variables that continuously genertate three different random numbers that get animated onto the screen. Create a variable to position the numbers vertically. Change the frame rate so that the numbers are not being displayed too quickly to see.
+// https://editor.p5js.org/andrewdoak/sketches/eWVkl3NRw
+
+// CHAPTER 6, SECTION D  -  Loops (noise() function)  Circles
+// Learn about noise function creating random values with smoother transitions. First, animate circles with random() function, a frenetic look. Then observe the very different (breathing-, wave-like) effect when using the noise() function.
+// https://editor.p5js.org/andrewdoak/sketches/wXY95NI3i
+
+// 7/26 PM (Post-PD Class) - ROADMAP
+// Finish Tutorial "Class" (MODULES 7-12)
+// Work on creating a basic Tic Tac Toe in p5 (my Backup)
+// STRETCH: get some boilerplate HTML/CSS and 'publish' either Tic Tac Toe, or a sketch to GitHub pages (I have learned how to do this, as well as how to publish the sketch on a site)
